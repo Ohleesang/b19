5484edbf689b8cf28d313f1871bc5530d907d8e8
Ohleesang <jammmmed@gmail.com>
2024-01-11 09:16:31 +0900

    나눗셈 문제

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,2 +1,16 @@
+//피자 7조각으로 나눠먹을때 필요한 피자수
+//7로 나누고 나머지가있다면 + 1
 class Solution {
+    fun solution(n: Int): Int {
+        val res = n / 7
+        val dec = n % 7
+        return if (dec == 0) res
+        else res + 1
+    }
+}
+
+fun main() {
+    Solution().solution(7)//1
+    Solution().solution(1)//1
+    Solution().solution(15)//3
 }
